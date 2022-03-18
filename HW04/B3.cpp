b4e0bd6c5105e8d9f95be8f707724c338a16a641
SnowShark <truongmualanh@gmail.com>
2022-03-18 18:00:52 +0700

    Sua lai

--- HW04/B3.cpp
+++ HW04/B3.cpp
@@ -6,10 +6,12 @@ int GCD(int a, int b) {
     if (b==0) return a;
     else return GCD(b,a%b);
 }
-
+int LCM(int a, int b) {
+    return (a*b)/GCD(a,b);
+}
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << GCD(a,b) << endl;
-    cout << "LCM: " << a*b/GCD(a,b);
+    cout << "Uoc chung lon nhat: " << GCD(a,b) << endl;
+    cout << "Boi chung nho nhat : " <<LCM(a,b);
 }
